d728ba5ae8032a66cb883cb64c01a123f9bbd775
wafley <alwafielgin@gmail.com>
2026-05-04 23:34:21 +0700

    feat(movie): improve movie show view layout and enhance information display

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -19,14 +19,14 @@
         <div class="col-lg-9">
             <div class="card custom-card">
                 <div class="card-header">
-                    <div>
-                        <h4 class="card-title">Informasi Film</h4>
-                        <small class="text-muted w-100">
-                            Terakhir diperbarui {{ formatDate($movie->updated_at) }}
-                        </small>
-                    </div>
-                    {{-- <div class="d-flex align-items-center justify-content-between w-100">
-                        
+                    <div class="d-flex align-items-center justify-content-between w-100">
+                        <div>
+                            <h4 class="card-title">Informasi Film</h4>
+                            <small class="text-muted w-100">
+                                Terakhir diperbarui {{ formatDate($movie->updated_at) }}
+                            </small>
+                        </div>
+
                         <div class="d-flex align-items-center gap-3">
                             <a href="{{ route('movies.edit', $movie->slug) }}" class="btn btn-info spa-link">
                                 <i class="bi bi-pencil-square"></i> Edit
@@ -35,54 +35,43 @@
                                 <i class="bi bi-trash"></i> Hapus
                             </button>
                         </div>
-                    </div> --}}
+                    </div>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <h3 class="text-uppercase fw-bold text-primary">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <h3 class="text-uppercase fw-bold text-primary mb-0">
                             {{ $movie->title }}
                         </h3>
 
-                        <p>
-                            <strong>Durasi:</strong>
-                            <span class="fst-italic fs-underline">{{ $movie->duration }}</span>
-                        </p>
-
-                        <p>{{ $movie->description }}</p>
-
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">
-                                <strong>Sutradara:</strong>
-                                <span class="fst-italic">{{ $movie->director }}</span>
-                            </li>
-                            <li class="list-group-item">
-                                <strong>Genre:</strong>
-                                @foreach ($movie->genre as $genre)
-                                    <span class='badge text-bg-primary'>{{ $genre }}</span>
-                                @endforeach
-                            </li>
-                            <li class="list-group-item">
-                                <strong>Pemeran:</strong>
-                                @foreach ($movie->cast as $cast)
-                                    <span class='badge text-bg-primary'>{{ $cast }}</span>
-                                @endforeach
-                            </li>
-                            <li class="list-group-item">
-                                <strong>Status:</strong>
-                                <span class='badge text-bg-{{ $movie->status_color }}'>{{ $movie->status_label }}</span>
-                            </li>
-                        </ul>
+                        <span class='badge text-bg-{{ $movie->status_color }}'>{{ $movie->status_label }}</span>
                     </div>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex align-items-center gap-3">
-                        <a href="{{ route('movies.edit', $movie->slug) }}" class="btn btn-info w-100 spa-link">
-                            <i class="bi bi-pencil-square"></i> Edit
-                        </a>
-                        <button class="btn btn-danger w-100" data-ajax="delete" data-url="{{ route('movies.destroy', $movie->slug) }}">
-                            <i class="bi bi-trash"></i> Hapus
-                        </button>
+
+                    <div class="d-flex gap-2 align-items-center mb-3">
+                        <small class="text-muted">
+                            <i class="bi bi-clock me-1"></i>
+                            {{ $movie->duration }}
+                        </small>
+
+                        <small class="text-muted">
+                            <i class="bi bi-tags me-1"></i>
+                            {{ implode(', ', $movie->genre) }}
+                        </small>
                     </div>
+
+                    <p>{{ $movie->description }}</p>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <strong>Sutradara:</strong>
+                            <span class="fst-italic">{{ $movie->director }}</span>
+                        </li>
+                        <li>
+                            <strong>Pemeran:</strong>
+                            @foreach ($movie->cast as $cast)
+                                <span class='badge text-bg-secondary'>{{ $cast }}</span>
+                            @endforeach
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
