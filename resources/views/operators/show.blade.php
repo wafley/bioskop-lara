b6bf8aeadbd004aca6aea186886aadd06818256a
wafley <alwafielgin@gmail.com>
2026-05-07 20:05:35 +0700

    feat: enhance operator show view with improved layout and detailed information

--- resources/views/operators/show.blade.php
+++ resources/views/operators/show.blade.php
@@ -15,42 +15,56 @@
                         Terakhir diperbarui {{ formatDate($operator->updated_at) }}
                     </small>
                 </div>
-                <div class="card-body pb-0">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <strong>Nama:</strong>
-                            <span class="float-end mark">{{ $operator->name }}</span>
-                        </li>
-                        <li class="list-group-item">
-                            <strong>Username:</strong>
-                            <span class="float-end font-monospace">{{ $operator->username }}</span>
-                        </li>
-                        <li class="list-group-item">
-                            <strong>Role:</strong>
-                            <span class="float-end fst-italic">{{ $operator->role->label }}</span>
-                        </li>
-                        <li class="list-group-item">
-                            <strong>Status Akun:</strong>
-                            <span class="float-end">
-                                <span class="badge text-bg-{{ $operator->status_color }}">
-                                    {{ $operator->status_label }}
-                                </span>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex align-items-center justify-content-end gap-3">
-                        <a href="{{ route('operators.edit', $operator->username) }}" class="btn btn-info spa-link">
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center text-center mb-3">
+                        <div class="avatar avatar-xxl mb-3">
+                            <img src="{{ asset('assets/images/placeholders/profile-placeholder.jpg') }}" alt="{{ $operator->name }}" class="rounded-circle img-fluid">
+                        </div>
+
+                        <h5 class="mb-1">{{ $operator->name }}</h5>
+                        <span class="">{{ ucfirst($operator->role->label) }}</span>
+                    </div>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <a href="{{ route('operators.edit', $operator->username) }}" class="btn btn-info spa-link w-100">
                             <i class="bi bi-pencil-square"></i> Edit
                         </a>
-                        <button class="btn btn-danger" data-ajax="delete" data-url="{{ route('operators.destroy', $operator->username) }}">
+                        <button class="btn btn-danger w-100" data-ajax="delete" data-url="{{ route('operators.destroy', $operator->username) }}">
                             <i class="bi bi-trash"></i> Hapus
                         </button>
+                    </div>
+
+                    <hr />
+
+                    <div class="table-responsive">
+                        <table class="table table-borderless mb-0">
+                            <tbody>
+                                <tr>
+                                    <th width="25%" class="text-muted fw-bold">Username</th>
+                                    <td class="font-monospace">: {{ $operator->username }}</td>
+                                </tr>
+                                <tr>
+                                    <th class="text-muted fw-bold">Status</th>
+                                    <td class="font-monospace">
+                                        :
+                                        @if ($operator->is_active)
+                                            <span class="badge bg-success">Aktif</span>
+                                        @else
+                                            <span class="badge bg-danger">Nonaktif</span>
+                                        @endif
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th class="text-muted fw-bold">Dibuat Pada</th>
+                                    <td class="font-monospace">: {{ formatDate($operator->created_at) }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-7">
             <div class="card custom-card">
                 <div class="card-header">
